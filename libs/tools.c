26c9bfe8f7643158c2ba32e8d4ac1b857634b60e
jleveau <julien.leveau@etu.u-bordeaux1.fr>
2015-04-10 16:42:42 +0200

    update tools.c

--- libs/tools.c
+++ libs/tools.c
@@ -25,7 +25,7 @@ void create_variable(char* nom, char* description){
 	variable v=malloc(sizeof(*v));
 	v->nom=nom;
 	v->description=description;
-	list l=stack_top(id_manager->variables_stack);
+	list l=stack_top(variables_stack);
 
 	add_to_list(l,v);
 }
@@ -39,16 +39,9 @@ void add_to_list(list l,void* elem){
 }
 
 void new_block(){
-	stack_push(id_manager->typedef_stack,list_create());
-	stack_push(id_manager->variables_stack,list_create());
+	stack_push(variables_stack,list_create());
 }
 
-void init_id_manager(){
-	id_manager=malloc(sizeof(*id_manager));
-	id_manager->typedef_stack=stack_create();
-	id_manager->variables_stack=stack_create();
-	new_block();
-}
 
 
 void print_variable(variable v){
@@ -57,7 +50,7 @@ void print_variable(variable v){
 
 // affiche la liste au sommet de la pile de variables
 void print_variables(){
-	list l=(list)stack_top(id_manager->variables_stack);
+	list l=(list)stack_top(variables_stack);
 	while (l->first!=NULL){
 		print_variable((variable)l->first->elem);
 		l->first=l->first->next;
